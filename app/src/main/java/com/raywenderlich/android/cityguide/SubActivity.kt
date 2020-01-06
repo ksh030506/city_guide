11698773c1305dbd6a3de037f16dd9b732fb2daf
sanghyun0 <sanghyun0@github.com>
2020-01-06 20:21:46 +0900

    coommit

--- app/src/main/java/com/raywenderlich/android/cityguide/SubActivity.kt
+++ app/src/main/java/com/raywenderlich/android/cityguide/SubActivity.kt
@@ -3,6 +3,7 @@ package com.raywenderlich.android.cityguide
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.CalendarView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sub.*
 
@@ -17,7 +18,7 @@ class SubActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        calendarView?.setOnDateChangeListener { view, year, month, dayOfMonth ->
+        calendarView?.setOnDateChangeListener { _: CalendarView, year: Int, month: Int, dayOfMonth: Int ->
             val msg:String = year.toString() + month.toString() + dayOfMonth.toString()
             runOnUiThread {
                 Toast.makeText(this,msg,Toast.LENGTH_SHORT).show() //날짜 터치시 알림표시로 나타내기
